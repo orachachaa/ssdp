0633f1375e19301740bf01106d2f69af8294e914
ecourse <smkang@codenuri.co.kr>
2024-07-22 10:37:24 +0900

    Add files via upload

--- DAY1/2_upcasting2.cpp
+++ DAY1/2_upcasting2.cpp
@@ -10,5 +10,5 @@ int main()
 	// upcasting 활용 #1. 
 	std::vector<Dog*>    v1; // v1 에는 Dog 객체만 저장하겠다는 의도
 	std::vector<Animal*> v2; // v2 에는 모든 동물 객체를 저장하겠다는 의도
-							 // "동종을 보관하는 컨테이너 만들기"
+							 // "동종(Animal 에서 파생된 클래스)을 보관하는 컨테이너 만들기"
 }
