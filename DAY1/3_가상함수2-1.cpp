8c33355858b99bfe53cc51cfc15f9f5bc4e757e4
ecourse <smkang@codenuri.co.kr>
2024-07-22 12:50:27 +0900

    Add files via upload

---
+++ DAY1/3_가상함수2-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class Base
+{
+public:
+	virtual ~Base() {}
+};
+
+class Derived : public Base
+{
+public:
+	Derived() { std::cout << "Derived() 자원할당" << std::endl; }
+	~Derived() { std::cout << "~Derived() 자원해지" << std::endl; }
+};
+int main()
+{
+	Base* p = new Derived;
+	delete p;	
+
+}
