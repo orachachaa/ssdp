66b527087553d4404571c68a4e73fa1a07cdb231
ecourse <smkang@codenuri.co.kr>
2024-07-22 16:20:57 +0900

    Add files via upload

--- DAY1/6_Edit2.cpp
+++ DAY1/6_Edit2.cpp
@@ -7,10 +7,18 @@
 // => Validation 정책은 사용자가 변경할수 있어야 한다.
 // => Edit 는 라이브러리 클래스 이므로 Edit 내부 코드를 사용자가 변경할수는 없다
 
+// 방법 #1. 변해야 하는 것을 "가상함수"로 분리
+
+
 class Edit
 {
 	std::string data;
 public:
+
+	virtual bool validate(char c)
+	{
+		return true;
+	}
 	std::string get_data()
 	{
 		data.clear();
@@ -21,7 +29,8 @@ public:
 
 			if (c == 13) break; 
 
-			if (isdigit(c))  
+//			if (isdigit(c))		// 직접 validation
+			if (validate(c))	// 가상함수에 의존
 			{
 				data.push_back(c);
 				std::cout << c;
@@ -31,10 +40,24 @@ public:
 		return data;
 	}
 };
+// 위처럼 설계한 의도!!
+// => Validation 정책을 변경하려면
+// => 파생 클래스를 만들어서 "약속된 가상함수(validate) 를 override 하라는것
+// => 기존 코드의 수정이 아닌 기존 코드(라이브러리)의 확장
+
+class NumEdit : public Edit
+{
+public:
+	bool validate(char c) override
+	{
+		return isdigit(c);
+	}
+};
 
 int main()
 {
-	Edit e;
+//	Edit e;
+	NumEdit e;
 	while (1)
 	{
 		std::cout << e.get_data() << std::endl;
