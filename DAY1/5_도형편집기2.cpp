0d19481dffb471dd67d5a4627bedaf934c934e04
ecourse <smkang@codenuri.co.kr>
2024-07-22 14:11:55 +0900

    Add files via upload

---
+++ DAY1/5_도형편집기2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+class Shape
+{
+	int color;
+public:
+	virtual ~Shape() {}
+};
+
+class Rect : public Shape
+{
+public:
+	void draw() { std::cout << "draw Rect\n"; }
+};
+
+class Circle : public Shape
+{
+public:
+	void draw() { std::cout << "draw Circle\n"; }
+};
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	while (1)
+	{
+		int cmd;
+
+		std::cin >> cmd;
+
+		if      (cmd == 1) v.push_back(new Rect);
+		else if (cmd == 2) v.push_back(new Circle);
+		else if (cmd == 9)
+		{
+			for (auto s : v)
+				s->draw();
+		}
+	}
+}
+// 위 코드는 왜 에러일까요 ?
+// => 
+// 해결책은 뭘까요 ?
+// => 
+
