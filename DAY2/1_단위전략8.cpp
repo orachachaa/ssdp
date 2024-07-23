434e9ff6a331bd0992f04739ab963caee6ac859f
ecourse <smkang@codenuri.co.kr>
2024-07-23 10:14:51 +0900

    Add files via upload

--- DAY2/1_단위전략8.cpp
+++ DAY2/1_단위전략8.cpp
@@ -5,6 +5,7 @@
 // std::basic_string 입니다. template 입니다
 // => cppreference.com 에서 "std::string 검색"
 
+/*
 template<typename T,		// Ansi, unicode
 		 typename Traits = std::char_traits<T>, // 문자열 비교정책 클래스
 		 typename Alloc = std::allocator<T>>
@@ -21,17 +22,76 @@ public:
 	}
 };
 
+
 using string = std::basic_string<char>;
 using wstring = std::basic_string<wchar_t>;
 
+*/
+
+/*
+struct MyTraits
+{
+	// compare static 함수 있어야 합니다.
+	// 그외 규칙도 지켜야 합니다.
+};
+*/
+
+struct MyTraits : public std::char_traits<char>
+{
+    static char to_upper(char ch)
+    {
+        return std::toupper((unsigned char)ch);
+    }
+
+    static bool eq(char c1, char c2)
+    {
+        return to_upper(c1) == to_upper(c2);
+    }
+
+    static bool lt(char c1, char c2)
+    {
+        return to_upper(c1) < to_upper(c2);
+    }
+
+    static int compare(const char* s1, const char* s2, std::size_t n)
+    {
+        while (n-- != 0)
+        {
+            if (to_upper(*s1) < to_upper(*s2))
+                return -1;
+            if (to_upper(*s1) > to_upper(*s2))
+                return 1;
+            ++s1;
+            ++s2;
+        }
+        return 0;
+    }
+
+    static const char* find(const char* s, std::size_t n, char a)
+    {
+        const auto ua{ to_upper(a) };
+        while (n-- != 0)
+        {
+            if (to_upper(*s) == ua)
+                return s;
+            s++;
+        }
+        return nullptr;
+    }
+};
+
+
+
+
+using ci_string = std::basic_string<char, MyTraits>;
 
 
 
 
 int main()
 {
-	std::string s1 = "abcd";
-	std::string s2 = "ABCD";
+    ci_string s1 = "abcd";
+    ci_string s2 = "ABCD";
 
 	std::cout << (s1 == s2) << std::endl; 
 }
