5f79ff46c30d1f503d6909c8e34b552fa75678d9
ecourse <smkang@codenuri.co.kr>
2024-07-23 13:47:09 +0900

    Add files via upload

---
+++ DAY2/function1.cpp
@@ -0,0 +1,16 @@
+// function1.cpp
+// => 메뉴 예제 완성을 위한 사전 지식 설명
+#include <functional>
+#include <iostream>
+
+
+void foo(int a, int b int c, int d)
+{
+	printf("%d, %d, %d, %d\n", a, b, c, d);
+}
+
+int main()
+{
+	foo(1, 2, 3, 4);  // foo는 4항 함수
+
+}
