2e61c10edeaacbc1ae76e5d2a4487f7cfd4e8154
ecourse <smkang@codenuri.co.kr>
2024-07-23 10:09:27 +0900

    Add files via upload

--- DAY2/1_단위전략7.cpp
+++ DAY2/1_단위전략7.cpp
@@ -1,10 +1,12 @@
 #include <string>
 #include <iostream>
 
+
 int main()
 {
 	std::string s1 = "abcd";
 	std::string s2 = "ABCD";
 
-	std::cout << (s1 == s2) << std::endl; // 0 ? 1
+	std::cout << (s1 == s2) << std::endl; // 0 
+										  // Áï, case sensitive
 }
--- DAY2/1_단위전략8.cpp
+++ DAY2/1_단위전략8.cpp
@@ -5,15 +5,25 @@
 // std::basic_string 입니다. template 입니다
 // => cppreference.com 에서 "std::string 검색"
 
-template<typename T,
-		 typename Traits = std::char_traits<T>, 
+template<typename T,		// Ansi, unicode
+		 typename Traits = std::char_traits<T>, // 문자열 비교정책 클래스
 		 typename Alloc = std::allocator<T>>
 class basic_string
 {
 	T* ptr;	  // 문자열이 있는 메모리
 	Alloc ax; // 메모리 할당기
 public:
+	bool operator==(const std::basic_string& other)
+	{
+		// 결국 문자열 비교는 직접하지 않고
+		// 비교 정책 클래스의 static 멤버 함수를 사용합니다.
+		return Traits::compare(ptr, other.ptr);
+	}
 };
+
+using string = std::basic_string<char>;
+using wstring = std::basic_string<wchar_t>;
+
 
 
 
