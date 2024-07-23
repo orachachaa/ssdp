19bfa56f7679ddb46cfefa5fa1fa1de61084f67c
ecourse <smkang@codenuri.co.kr>
2024-07-23 09:43:44 +0900

    Add files via upload

--- DAY2/1_단위전략6.cpp
+++ DAY2/1_단위전략6.cpp
@@ -5,13 +5,35 @@
 // 1. 반드시 template 일 필요는 없지만, 다양한 타입에 사용하기 위해
 //    대부분 템플릿으로 작성
 
+// 2. 반드시 표준 문서에서 약속된 규칙을 지켜야 합니다.
+// => 인터페이스로 약속하지 않고(가상함수를 사용하지 않기 위해)
+// => 문서화로 약속되어 있습니다
+// => allocate, deallocate 가 있어야 합니다.
+
 template<typename T>
 class debug_alloc
 {
 public:
+	inline T* allocate(std::size_t size) 
+	{ 
+		T* p = static_cast<T*>(malloc(sizeof(T) * size)); 
+		printf("allocate : %p, %zd\n", p, size);
+		return p;
+	}
 
+	inline void deallocate(T* p, std::size_t size)
+	{ 
+		printf("deallocate : %p, %zd\n", p, size);
+		free(p); 
+	}
+
+	// 위 2개 외에도 아래 3개가 필요 합니다
+	// => 관례적인 코드이므로 복사해서 사용하면됩니다.
+
+	debug_alloc() {}
+	template<typename U> debug_alloc(const debug_alloc<U>&) {}
+	using value_type = T;
 };
-
 
 
 int main()
