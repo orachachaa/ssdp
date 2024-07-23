dda1aba189099088901df5a7187a866dc796de24
ecourse <smkang@codenuri.co.kr>
2024-07-23 15:38:13 +0900

    Add files via upload

--- DAY2/4_Decorator5.cpp
+++ DAY2/4_Decorator5.cpp
@@ -2,7 +2,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 
-class FileStream
+// 다양한 Stream 의 함수이름을 약속하기 위한 인터페이스
+
+struct Stream
+{
+	virtual void write(const std::string&) = 0;
+	virtual ~Stream() {}
+
+	// 이외에도 read, flush, is_open 등의 다양한 함수를 여기서 약속
+};
+
+
+class FileStream : public Stream
 {
 	FILE* file;
 public:
@@ -22,4 +33,13 @@ int main()
 {
 	FileStream fs("a.txt");
 	fs.write("hello");
+
+	/*
+	NetworkStream ns("100.100.100.100", 3000);
+	ns.write("hello");
+
+
+	PipeStream ps("mynamedpipe");
+	ps.write("hello");
+	*/
 }
