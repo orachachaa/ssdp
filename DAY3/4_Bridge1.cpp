1d1925ac042238886fefcc09097bab71d250929e
user <user@DESKTOP-4GV3OSK>
2024-07-24 08:33:10 +0900

    7/24 day3 init

---
+++ DAY3/4_Bridge1.cpp
@@ -0,0 +1,27 @@
+﻿// 6_Bridge1 - 90 page
+#include <iostream>
+
+class IPod 
+{
+public:
+	void play() { std::cout << "Play MP3 with IPod" << std::endl; }
+	void stop() { std::cout << "Stop" << std::endl; }
+};
+
+class People
+{
+public:
+	void use(IPod* p)
+	{
+		p->Play();
+		p->Stop();
+	}
+};
+int main()
+{
+	People p;
+	IPod pod;
+	p.Use(&pod);
+}
+
+
