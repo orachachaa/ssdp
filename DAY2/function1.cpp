3e95034c5f7ecdd39f88d70049a704afde82ecfd
ecourse <smkang@codenuri.co.kr>
2024-07-23 13:49:05 +0900

    Add files via upload

--- DAY2/function1.cpp
+++ DAY2/function1.cpp
@@ -3,7 +3,6 @@
 #include <functional>
 #include <iostream>
 
-
 void foo(int a, int b int c, int d)
 {
 	printf("%d, %d, %d, %d\n", a, b, c, d);
@@ -12,5 +11,12 @@ void foo(int a, int b int c, int d)
 int main()
 {
 	foo(1, 2, 3, 4);  // foo는 4항 함수
+
+	// std::bind : 인자를 고정한 새로운 함수를 만드는 도구
+	
+	// 사용법     : std::bind(M항함수주소, M개인자)
+
+	auto f1 = std::bind(&foo, 1, 2, 3, 4);
+	f1();
 
 }
