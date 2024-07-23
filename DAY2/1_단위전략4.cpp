2cceb64dc9ab20489fce5777ce85c6dbf6043bb0
ecourse <smkang@codenuri.co.kr>
2024-07-23 09:12:39 +0900

    Add files via upload

--- DAY2/1_단위전략4.cpp
+++ DAY2/1_단위전략4.cpp
@@ -6,6 +6,11 @@
 // 실행시간에 다른 할당기로 교체 하려면 "약한 결합"이  되어야 한다.
 // 그런데, 인터페이스를 만들면 "가상함수" 일수 밖에 없다.
 
+// 아래 처럼 강한 결합(vector 가 "MallocAllocator" 라는 이름을 직접사용)을 사용하면
+// => allocate 와 deallocate 를 inline 으로 하므로 성능 향상이 있다.
+
+// 그런데, 강한 결합 이므로 교체 할수 없다.
+
 template<typename T>
 class MallocAllocator
 {
