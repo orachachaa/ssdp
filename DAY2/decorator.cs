541a972a81c3549f67e82cbedd1b4b57431b8e17
ecourse <smkang@codenuri.co.kr>
2024-07-23 16:07:41 +0900

    Add files via upload

--- DAY2/decorator.cs
+++ DAY2/decorator.cs
@@ -1,11 +1,18 @@
 ﻿
+using System.IO.Compression;
+
 byte[] arr = new byte[1024 * 1024]; // 1M 크기
 
 FileStream fs = new FileStream("a.dat", FileMode.Create);
 
-fs.Write(arr, 0, arr.Length );
+GZipStream zip = new GZipStream(fs, CompressionLevel.SmallestSize);
+
+//fs.Write(arr, 0, arr.Length );
+zip.Write(arr, 0, arr.Length);
 
 fs.Close();
 
-
+// project 이름/bin/debug/net8.0/a.dat 크기 확인
+// => 압축 안하면  1M
+// => 압축 하면    10byte
 
