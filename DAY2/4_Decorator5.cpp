40737c9e2e6a1bbc7ae2d085b4fd0552e24e44ad
ecourse <smkang@codenuri.co.kr>
2024-07-23 15:33:32 +0900

    Add files via upload

---
+++ DAY2/4_Decorator5.cpp
@@ -0,0 +1,25 @@
+﻿// visual studio 에서 fopen, scanf 등을 사용할때 아래 매크로 필요
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+class FileStream
+{
+	FILE* file;
+public:
+	FileStream(const char* s, const char* mode = "wt")
+	{
+		file = fopen(s, mode);
+	}
+	~FileStream() { fclose(file); }
+
+	void write(const std::string& s) 
+	{
+		printf("%s 쓰기\n", s.c_str() );
+	}
+};
+
+int main()
+{
+	FileStream fs("a.txt");
+	fs.write("hello");
+}
