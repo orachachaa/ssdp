046cbd317e41802078abcd41e11ea0fcb8f07d43
ecourse <smkang@codenuri.co.kr>
2024-07-24 10:21:56 +0900

    Add files via upload

--- DAY3/CalcProxy.cpp
+++ DAY3/CalcProxy.cpp
@@ -1,12 +1,23 @@
 #define USING_GUI
+#include <atomic>
 #include "cppmaster.h"
 #include "ICalc.h"
 
 class Calc : public ICalc
 {
+//	int refcnt = 0;
+	std::atomic<int> refcnt = 0; // 스레드 안전한 int
+
 	int server;
 public:
+
+	void AddRef()  { ++refcnt;  }
+	void Release() { if (--refcnt == 0) delete this; }
+
 	Calc() { server = ec_find_server("Calc"); }
+
+	~Calc() { std::cout << "~Calc\n"; }
+
 
 	int Add(int a, int b) { return ec_send_server(server, 1, a, b); };
 	int Sub(int a, int b) { return ec_send_server(server, 2, a, b); };
