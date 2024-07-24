2299b07dab6fa3e65094d724d1606f888b54c6ee
ecourse <smkang@codenuri.co.kr>
2024-07-24 10:53:08 +0900

    Add files via upload

--- DAY3/2_RemoteProxy8.cpp
+++ DAY3/2_RemoteProxy8.cpp
@@ -19,6 +19,12 @@ public:
 		if (obj != nullptr)
 			obj->Release();
 	}
+
+	// Raw Pointer 처럼 사용할수 있도록 하기 위해서 -> 와 * 연산자 재정의
+	// => 스마트 포인터의 핵심!!
+	// => 스마트 포인터는 결국 "smart proxy" 입니다
+	T* operator->() { return obj; }
+	T& operator*() { return *obj; }
 };
 
 int main()
@@ -26,6 +32,15 @@ int main()
 	// calc1, calc2는 포인터가 아닌 객체입니다
 	sp<ICalc> calc1 = load_proxy();
 	sp<ICalc> calc2 = calc1;
+
+	// calc1, calc2 는 "객체"지만 "포인터의도로 사용하므로"
+	// -> 와 * 가 되어야 합니다.
+
+	int n1 = calc1->Add(1, 2);
+	int n2 = (*calc2).Add(1, 2);
+
+	std::cout << n1 << ", " << n2 << std::endl;
+	
 }
 
 
