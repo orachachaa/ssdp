9c3fbadc9e525815f64d753cc728c427c159d628
ecourse <smkang@codenuri.co.kr>
2024-07-24 16:51:21 +0900

    Add files via upload

--- DAY3/CRTP1.cpp
+++ DAY3/CRTP1.cpp
@@ -5,22 +5,27 @@ class Window
 {
 public:
 	// 발생된 이벤트를 조사해서 약속된 함수 호출
-	void event_loop()
+	void event_loop()	// void event_loop(Window* this)
 	{
-		on_click();
+		on_click();		// this->on_click(); 로 변경되는데..
+						//					객체는 MainWindow 이지만
+						//					this 가 Window* 이고
+						//					on_click() 가 non-virtual 이므로
+						//					1번호출.
+						//					2번을 호출하려면 가상이어야 한다.		
 	}
-	void on_click() { std::cout << "window on_click\n"; }
+	void on_click() { std::cout << "window on_click\n"; }		// 1
 };
 
 // 위 라이브러리의 사용자 코드
 class MainWindow : public Window
 {
 public:
-	void on_click() { std::cout << "MainWindow on_click\n"; }
+	void on_click() { std::cout << "MainWindow on_click\n"; }	// 2
 };
 
 int main()
 {
 	MainWindow w;
-	w.event_loop();
+	w.event_loop(); // event_loop(&w) 로 변경
 }
