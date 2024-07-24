c6aba012f25e9faabfa2370c7b56ff47e2ac57b0
ecourse <smkang@codenuri.co.kr>
2024-07-24 13:10:50 +0900

    Add files via upload

---
+++ DAY3/4_Bridge3.cpp
@@ -0,0 +1,34 @@
+﻿// Point.h
+class Point
+{
+	int x, y;
+
+	int debug; // 나중에 변경되었다.
+
+public:
+	void print();
+};
+
+
+
+// Point.cpp
+#include "Point.h"
+
+void Point::print() { } // 구현 생략
+
+
+
+
+
+
+
+
+// main.cpp
+#include "Point.h"
+int main()
+{
+	Point p;
+	p.print();
+}
+
+
