b61e4ff67fde09a36dff3e7df6665546b41c8291
ecourse <smkang@codenuri.co.kr>
2024-07-24 16:40:30 +0900

    Add files via upload

--- DAY3/6_싱글톤7.cpp
+++ DAY3/6_싱글톤7.cpp
@@ -11,12 +11,19 @@ private:
 	Cursor& operator=(const Cursor&) = delete;
 
 public:
+
+	// 아래 코드는 C++11 이후 부터는 멀티스레드 환경에서 안전합니다.
+	// => 동기화 됩니다.
+	// => 동기화 방법 "spin lock" 
 	static Cursor& get_instance()
 	{
 		static Cursor instance;
+
 		return instance;
 	}
 };
+
+
 int main()
 {
 	Cursor& c1 = Cursor::get_instance();
