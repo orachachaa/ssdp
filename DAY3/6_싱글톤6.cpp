126446e90d84cfd666266f2c5eb1798c99e6a1c3
ecourse <smkang@codenuri.co.kr>
2024-07-24 16:19:04 +0900

    Add files via upload

--- DAY3/6_싱글톤6.cpp
+++ DAY3/6_싱글톤6.cpp
@@ -1,24 +1,35 @@
 #include <iostream>
 #include <mutex>
 
+// CRTP : Curiously Recurring Template Pattern
+//        (신기하게도 언급되는 템플릿 패턴 ?)
 
-class Cursor
+// => 기반 클래스 만들때 파생 클래스 이름을 사용할수 있게 하는 패턴
+// => 기반 클래스를 템플릿으로 만들고, 파생 클래스 만들때 자신의 이름을
+//    기반 클래스 템플릿 인자로 전달하는 것
+
+// 예전에는 가벼운 테크닉이었는데, 지금은 아주 중요한 기술
+// =>C++20 의 많은 라이브러리가 이기술 사용.
+
+
+template<typename T>
+class Singleton
 {
-private:
-	Cursor() {}
-	Cursor(const Cursor& c) = delete;
-	Cursor& operator=(const Cursor&) = delete;
+protected:
+	Singleton() {}
 
-	inline static Cursor* sinstance = nullptr;
+	Singleton(const Singleton& c) = delete;
+	Singleton& operator=(const Singleton&) = delete;
+
+	inline static T* sinstance = nullptr;
 	inline static std::mutex mtx;
 public:
-
-	static Cursor& get_instance()
+	static T& get_instance()
 	{
 		std::lock_guard<std::mutex> g(mtx);
 
 		if (sinstance == nullptr)
-			sinstance = new Cursor;
+			sinstance = new T;
 
 		return *sinstance;
 	}
@@ -26,7 +37,7 @@ public:
 
 // Mouse 클래스도  Cursor처럼 힙에 만드는 싱글톤 모델을 사용하고 싶다.
 
-class Mouse
+class Mouse : public Singleton< Mouse >
 {
 
 };
