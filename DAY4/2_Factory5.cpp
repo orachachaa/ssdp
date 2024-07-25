81b9b536b71dd803c6ef5599078c92e6f210bfa4
ecourse <smkang@codenuri.co.kr>
2024-07-25 10:52:47 +0900

    Add files via upload

--- DAY4/2_Factory5.cpp
+++ DAY4/2_Factory5.cpp
@@ -69,8 +69,22 @@ int main()
 	ShapeFactory& factory = ShapeFactory::get_instance();
 
 	// 공장에는 제품(도형)을 등록후에 사용해야 합니다.
-	factory.register_shape(1, &Rect::create);
-	factory.register_shape(2, &Circle::create);
+	// => 아래 코드는 공장에 "생성함수"를 등록하고 있습니다.
+	// => 결국, "클래스" 를 등록하는 의미 입니다.
+//	factory.register_shape(1, &Rect::create);
+//	factory.register_shape(2, &Circle::create);
+
+
+	// 이번에는 "생성함수" 가 아닌
+	// => 자주 사용되는 견본객체를 공장에 등록해 봅시다.
+	Rect* redRect = new Rect;	
+	Rect* blueRect = new Rect;
+	Circle* redCircle = new  Circle;
+
+	factory.register_sample(1, redRect);
+	factory.register_sample(2, blueRect);
+	factory.register_sample(3, redCircle);
+
 
 
 	while (1)
