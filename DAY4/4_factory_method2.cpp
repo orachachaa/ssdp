ba1989be570d8ca2633cd87ff4913e2787a3724a
ecourse <smkang@codenuri.co.kr>
2024-07-25 13:01:59 +0900

    Add files via upload

---
+++ DAY4/4_factory_method2.cpp
@@ -0,0 +1,81 @@
+﻿// 5_팩토리메소드
+#include <iostream>
+
+// 모든 종류의 컨트롤은 공통의 기반 클래스 필요
+struct IButton
+{
+	virtual void Draw() = 0;
+	virtual ~IButton() {}
+};
+struct IEdit
+{
+	virtual void Draw() = 0;
+	virtual ~IEdit() {}
+};
+
+
+struct WinButton : public IButton
+{
+	void Draw() { std::cout << "Draw WinButton" << std::endl; }
+};
+struct WinEdit : public IEdit
+{
+	void Draw() { std::cout << "Draw WinEdit" << std::endl; }
+};
+
+struct OSXButton : public IButton
+{
+	void Draw() { std::cout << "Draw OSXButton" << std::endl; }
+};
+struct OSXEdit : public IEdit
+{
+	void Draw() { std::cout << "Draw OSXButton" << std::endl; }
+};
+//--------------------------------------
+// Style 옵션과 상관없이 항상 Windows 모양의 Dialog
+
+class DialogBase
+{
+public:
+	void init()
+	{
+		IButton* btn = CreateButton();
+		IEdit* edit = CreateEdit();
+
+		// btn->Move(); edit->Move();
+
+		btn->Draw();
+		edit->Draw();
+	}
+	// 객체를 만들기 위한 인터페이스를 제공하고 사용하지만
+	// 어떤 객체를 만들지(객체의 타입)는 파생 클래스가 결정한다.
+	// factory method 패턴
+	virtual IButton* CreateButton() = 0;
+	virtual IEdit*   CreateEdit() = 0;
+};
+
+class WinDialog : public DialogBase
+{
+public:
+	IButton* CreateButton() override { return new WinButton; }
+	IEdit* CreateEdit() override { return new WinEdit; }
+};
+class OSXDialog : public DialogBase
+{
+public:
+	IButton* CreateButton() override { return new OSXButton; }
+	IEdit* CreateEdit() override { return new OSXEdit; }
+};
+
+int main(int argc, char** argv)
+{
+
+}
+
+
+
+
+
+
+
+
