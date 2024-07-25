06df535a18c51de0fa152dbbe06093e843c33150
ecourse <smkang@codenuri.co.kr>
2024-07-25 14:26:10 +0900

    Add files via upload

--- DAY4/thiscall.cpp
+++ DAY4/thiscall.cpp
@@ -1,13 +1,19 @@
 // thiscall
+
+// 핵심 1. 멤버 함수는 인자로 this가 추가됩니다.
+//     2. static 멤버 함수 는 this 가 추가되지 않습니다.
+
 class Point
 {
 	int x, y;
 public:
-	void set(int a, int b)
+	void set(int a, int b)// void set(Point* this, int a, int b)
 	{
-		x = a;
-		y = b;
+		x = a;				// this->x = a;
+		y = b;				// this->y = b;
 	}
+
+	static void foo(int a, int b) {}
 };
 
 int main()
@@ -15,4 +21,13 @@ int main()
 	Point p1, p2;
 
 	p1.set(10, 20); // 원리를 정확히 알아 봅시다.
+					// set(&p1, 10, 20)
+
+	// 함수포인터와 멤버 함수
+	void(*f)(int, int) = &Point::set;	// error.
+										// set의 인자는 2개 아님!!
+										// this 추가됨
+
+	void(*f)(int, int) = &Point::foo;	// ok. 인자 2개 맞음.
+										// static 멤버 함수는 this 추가 안됨
 }
