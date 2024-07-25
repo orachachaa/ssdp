1ba167b0fe4787353ab0324ae956cae5453bc2b3
ecourse <smkang@codenuri.co.kr>
2024-07-25 12:45:26 +0900

    Add files via upload

---
+++ DAY4/3_abstract_factory1.cpp
@@ -0,0 +1,56 @@
+﻿// 3_추상팩토리 - 147
+#include <iostream>
+
+// 모든 종류의 컨트롤은 공통의 기반 클래스 필요
+struct IButton
+{
+	virtual void Draw() = 0;
+	virtual ~IButton() {}
+};
+struct IEdit
+{
+	virtual void Draw() = 0;
+	virtual ~IEdit() {}
+};
+
+
+struct WinButton : public IButton
+{
+	void Draw() { std::cout << "Draw WinButton" << std::endl; }
+};
+struct WinEdit : public IEdit
+{
+	void Draw() { std::cout << "Draw WinEdit" << std::endl; }
+};
+
+struct OSXButton : public IButton
+{
+	void Draw() { std::cout << "Draw OSXButton" << std::endl; }
+};
+struct OSXEdit : public IEdit
+{
+	void Draw() { std::cout << "Draw OSXButton" << std::endl; }
+};
+
+int main(int argc, char** argv)
+{
+	// 각 컨트롤은 기반 클래스(인터페이스)가 필요 하다.
+	IButton* btn;
+
+	if (strcmp(argv[1], "-style:OSX") == 0)
+		btn = new OSXButton;
+	else
+		btn = new WinButton;
+
+	// 그런데, 프로그램에서 "버튼은 한번만 만들게 될까 ??"
+	// 만들때 마다 위처럼 if 문을 사용한다면 ?
+
+}
+
+
+
+
+
+
+
+
