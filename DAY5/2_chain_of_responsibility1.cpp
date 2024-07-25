b78c5cca6f1e6e6452aad6115e63f51582ef9c09
ecourse <smkang@codenuri.co.kr>
2024-07-26 08:52:36 +0900

    Add files via upload

---
+++ DAY5/2_chain_of_responsibility1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+class Handler
+{
+	Handler* next = nullptr;
+
+public:
+	Handler* SetNext(Handler* h)
+	{
+		next = h;
+		return h;
+	}
+
+
+	void Handle(int problem)
+	{
+		if (HandleRequest(problem) == true)
+			return;
+
+		if (next != 0)
+			next->Handle(problem);
+	}
+
+	virtual bool HandleRequest(int problem) = 0;
+};
+
+int main()
+{
+}
