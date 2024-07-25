8e7c3aab118551048f70a1a30c62ff121b9028de
ecourse <smkang@codenuri.co.kr>
2024-07-25 09:38:19 +0900

    Add files via upload

--- DAY4/1_Flyweight7.cpp
+++ DAY4/1_Flyweight7.cpp
@@ -4,12 +4,32 @@
 // 인자로 받은 문자열을 읽기만 하려고 합니다(쓰기 안함)
 // => 최선의 코드를 찾아 봅시다
 
-void f1(std::string s) {}
+//void f1(std::string s) {} // worst,  복사본 생성
+
+void f2(const std::string& ss) {} // good, 단 best 는 아님.
+
+void f3(std::string_view sv)    {} // best
+
+
+
+
+
+
+
+
+
+
+
 
 int main()
 {
 	std::string s = "abcd";
 
+	// 아래 경우는 f2, f3  비슷합니다.
 	f2(s);
 	f3(s);
+
+	// 하지만 아래코드는 차이가 많이 있습니다.
+	f2("hello");
+	f3("hello");
 }
