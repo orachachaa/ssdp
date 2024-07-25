180a7c6bfe9809fdd72bf01e1fb976e479cf8b0c
ecourse <smkang@codenuri.co.kr>
2024-07-25 16:15:50 +0900

    Add files via upload

---
+++ DAY4/7_visitor2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <list>
+#include <vector>
+
+// #1. 방문자 인터페이스
+template<typename T>
+struct IVisitor
+{
+	virtual void visit(T& e) = 0;
+	virtual ~IVisitor() {}
+};
+
+// #2. 실제 Visitor 
+template<typename T> class TwiceVisitor : public IVisitor<T>
+{
+public:
+
+};
+
+
+
+int main()
+{
+	std::list<int> s = { 1,2,3,4,5,6,7,8,9,10 };
+
+
+	for (auto& e : s)
+		s *= 2;
+
+	TwiceVisitor<int> tv; 
+	s.accept(&tv);	  
+
+
+	ShowVisitor<int> sv;  
+	s.accept(&sv);	  
+}
+
+
+
+
+
