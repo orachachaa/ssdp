218d1c67403b2b24fda80ae9f6ff248b131ce28d
ecourse <smkang@codenuri.co.kr>
2024-07-25 09:07:13 +0900

    Add files via upload

---
+++ DAY4/1_Flyweight4.c
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+	// 문자열 배열 vs 문자열 포인터
+	// 문자열 배열   : 문자열 소유
+	// 문자열 포인터 : 문자열 소유 안함, 다른 문자열을 가리키는 것
+//	char sa[] = "abcd";
+//	char* sp = sa;
+
+	//----------------
+	char sa[] = "abcd";
+	char* sp = "abcd";
+
+	*sa = 'X'; // ok
+	*sp = 'X'; // runtime error
+
+	printf("main finish\n");
+}
+
+// 소스 확장자 .c 로 해 보세요
