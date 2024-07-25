ef79db9926c528fe981c5b9de15da140a6b556e8
ecourse <smkang@codenuri.co.kr>
2024-07-25 12:54:53 +0900

    Add files via upload

---
+++ DAY4/3_abstract_factory2.cpp
@@ -0,0 +1,92 @@
+﻿// 3_추상팩토리 - 147
+#include <iostream>
+
+// 모든 종류의 컨트롤은 공통의 기반 클래스 필요
+struct IButton
+{
+	virtual void Draw() = 0;
+	virtual ~IButton() {}
+};
+struct IEdit
+{
+	virtual void Draw() = 0;
+	virtual ~IEdit() {}
+};
+
+
+struct WinButton : public IButton
+{
+	void Draw() { std::cout << "Draw WinButton" << std::endl; }
+};
+struct WinEdit : public IEdit
+{
+	void Draw() { std::cout << "Draw WinEdit" << std::endl; }
+};
+
+struct OSXButton : public IButton
+{
+	void Draw() { std::cout << "Draw OSXButton" << std::endl; }
+};
+struct OSXEdit : public IEdit
+{
+	void Draw() { std::cout << "Draw OSXButton" << std::endl; }
+};
+//-----------------
+
+// 추상 팩토리 패턴 
+// => 제품의 군을 만들기 위한 인터페이스를 제공하지만
+// => 어떤 제품을 만들지는 하위 클래스가 결정한다.
+// => 공장도 인터페이스를 먼저 만들자는 의도
+
+struct IFactory
+{
+	virtual IButton* CreateButton() = 0;
+	virtual IEdit*   CreateEdit() = 0;
+	virtual ~IFactory() {}
+
+	// 그외에 다양한 컨트롤를 만드는 함수를 제공
+	// => combo, slider, scrollbar 등
+};
+
+
+//  style 별로 각 콘트롤을 만드는 공장
+class WinFactory : public IFactory
+{
+public:
+	IButton* CreateButton() { return new WinButton; }
+	IEdit*   CreateEdit()   { return new WinEdit; }
+	virtual ~WinFactory() {}
+};
+class OSXFactory : public IFactory
+{
+public:
+	IButton* CreateButton() { return new OSXButton; }
+	IEdit*   CreateEdit() { return new OSXEdit; }
+	virtual ~OSXFactory() {}
+};
+
+
+int main(int argc, char** argv)
+{
+	IFactory* factory;
+
+	if (strcmp(argv[1], "-style:OSX") == 0)
+		factory = new OSXFactory;
+	else
+		factory = new WinFactory;
+
+	// 컨트롤이 필요하면 공장을 사용해서 생성
+	IButton* btn = factory->CreateButton();
+	btn->Draw();
+}
+
+
+
+
+
+
+
+
+
+
+
