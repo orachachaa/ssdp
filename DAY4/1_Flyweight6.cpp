98bf49da833ad1cbd88b456bb2a5d23200f8d092
ecourse <smkang@codenuri.co.kr>
2024-07-25 09:25:24 +0900

    Add files via upload

--- DAY4/1_Flyweight6.cpp
+++ DAY4/1_Flyweight6.cpp
@@ -7,8 +7,10 @@ int main()
 	// string_view : const char* 와 동일. 단순히 가리키는것
 
 	std::string s1 = "abcd";
-	std::string s2 = s1;
+	std::string s2 = s1;	// 깊은복사(문자열 자원을 복사)
+
 	std::string_view sv = s1;
+
 
 	// 아래 2줄의 메모리 차이를 알아야 합니다.
 	std::string      ss1 = "abcd";
