dd143cf04cde9ca1335981c66876ecb8652fec85
ecourse <smkang@codenuri.co.kr>
2024-07-26 15:36:51 +0900

    Add files via upload

--- DAY5/5_range5.cpp
+++ DAY5/5_range5.cpp
@@ -2,6 +2,12 @@
 #include <iostream>
 #include <ranges> // C++20 부터 추가
 
+/*
+take_view operator | (vector, take(3)의반환타입)
+{
+	return take_view(v, take(3)반환타입안에있는 3이라는 정보 꺼내기)
+}
+*/
 int main()
 {
 	std::vector<int> v = { 1,2,3,4,5,6 };
@@ -14,7 +20,8 @@ int main()
 	auto rv2 = v | std::views::take(3) | std::views::reverse;
 
 
-	for (auto e : rv)
+//	for (auto e : rv)
+	for (auto e : rv2)
 	{
 		std::cout << e << std::endl;
 	}
