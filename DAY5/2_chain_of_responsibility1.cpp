75da77dc652ba8ebcbe3b60d8fde8909a9325d1e
ecourse <smkang@codenuri.co.kr>
2024-07-26 09:01:18 +0900

    Add files via upload

--- DAY5/2_chain_of_responsibility1.cpp
+++ DAY5/2_chain_of_responsibility1.cpp
@@ -47,6 +47,48 @@ public:
 	}
 };
 
+class Team2 : public Handler
+{
+public:
+	bool HandleRequest(int problem) override
+	{
+		std::cout << "Start Team2\n";
+
+		if (problem % 2 == 1)
+		{
+			std::cout << "Resolved by Team2\n";
+			return true;
+		}
+		std::cout << "End Team2\n";
+
+		return false;
+	}
+};
+
+class Team3 : public Handler
+{
+public:
+	bool HandleRequest(int problem) override
+	{
+		std::cout << "Start Team3\n";
+
+		if (problem < 10 )
+		{
+			std::cout << "Resolved by Team3\n";
+			return true;
+		}
+		std::cout << "End Team3\n";
+
+		return false;
+	}
+};
 int main()
 {
+	Team1 t1;
+	Team2 t2;
+	Team3 t3;
+
+	t1.SetNext(&t2)->SetNext(&t3);
+
+	t1.Handle(7);
 }
