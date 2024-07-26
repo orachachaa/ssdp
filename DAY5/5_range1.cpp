f35c9c83a967917024aee1e5a440a7b7462b425e
ecourse <smkang@codenuri.co.kr>
2024-07-26 15:15:04 +0900

    Add files via upload

--- DAY5/5_range1.cpp
+++ DAY5/5_range1.cpp
@@ -1,5 +1,4 @@
 // 5_range1.cpp
-
 #include <vector>
 #include <iostream>
 
@@ -10,5 +9,16 @@ int main()
 	for (auto e : v)
 	{
 		std::cout << e << std::endl;
+	}
+
+	// 위 for 문의 원리
+	auto first = v.begin();
+	auto last = v.end();
+
+	for (; first != last; ++first)
+	{
+		auto e = *first;
+		//-----------------------------
+		// 이 위치에 사용자 코드
 	}
 }
