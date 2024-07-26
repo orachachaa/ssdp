e9eab61fc203ac752bbd10c126d2a4b71e4c2a09
ecourse <smkang@codenuri.co.kr>
2024-07-26 09:40:55 +0900

    Add files via upload

--- DAY5/3_GUI3.cpp
+++ DAY5/3_GUI3.cpp
@@ -1,24 +1,46 @@
 #define USING_GUI
 #include "cppmaster.h"
+#include <map>
 
 
-
-//------------------------
 class Window
 {
 	int handle;
+
+	inline static map<int, Window*> this_map;	// 핵심
+												// QT, MFC, wxWidget등
+												// 대부분의 GUI 라이브러리
+												// 가 내부적으로 사용하는 기술
+							// [윈도우번호, this] 를 보관하는 자료구조
+
 public:
 	void create(const std::string& title)
 	{
 		handle = ec_make_window(msgproc, title);
+
+		// this 를 자료구조에 보관합니다.
+		// => static 멤버 함수에서 "키값 인 윈도우 핸들" 만 알수 있다면
+		//    this 를 사용할수 있게 됩니다.
+		this_map[handle] = this;
 	}
 
 	static int msgproc(int hwnd, int msg, int a, int b)
 	{
+		Window* self = this_map[hwnd];
+
+		// 이제 self 가 this 이므로
+		// 모든 멤버 데이타, 멤버 함수에 접근 가능합니다.
+
 		switch (msg)
 		{
-		case WM_LBUTTONDOWN: on_lbutton_down(); break;
-		case WM_KEYDOWN:     on_key_down(); break;
+		case WM_LBUTTONDOWN: 
+//			on_lbutton_down();  // this->on_lbutton_down();
+			self->on_key_down();
+			break;
+
+		case WM_KEYDOWN:     
+			self->on_key_down();
+			break;
 		}
 		return 0;
 	}
