f0784002584e22bcf1ef89ac664e0190448b1ae5
ecourse <smkang@codenuri.co.kr>
2024-07-26 15:22:32 +0900

    Add files via upload

--- DAY5/5_range3.cpp
+++ DAY5/5_range3.cpp
@@ -1,12 +1,26 @@
 #include <vector>
 #include <iostream>
 
+template<typename T>
+class take_view
+{
+	T& r;
+	int cnt;
+public:
+	take_view(T& r, int cnt) : r(r), cnt(c) {}
+
+	auto begin() { return r.begin(); }
+	auto end() { return r.begin() + ?; }
+};
 
 int main()
 {
 	std::vector<int> v = { 1,2,3,4,5,6 };
 
-	for (auto e : v) 
+	take_view<std::vector<int>> tv(v, 3);
+
+//	for (auto e : v) 
+	for (auto e : tv)
 	{
 		std::cout << e << std::endl;
 	}
