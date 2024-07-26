3d7e537da6c3dbf2d2accd6a8c6fb0a39d1b69c1
ecourse <smkang@codenuri.co.kr>
2024-07-26 09:31:06 +0900

    Add files via upload

--- DAY5/3_GUI2.cpp
+++ DAY5/3_GUI2.cpp
@@ -2,15 +2,7 @@
 #include "cppmaster.h"
 
 
-int foo(int hwnd, int msg, int a, int b)
-{
-	switch (msg)
-	{
-	case WM_LBUTTONDOWN: std::cout << "LBUTTON" << hwnd << std::endl; break;
-	case WM_KEYDOWN:     std::cout << "KEYDOWN" << hwnd << std::endl; break;
-	}
-	return 0;
-}
+
 //------------------------
 class Window
 {
@@ -18,7 +10,19 @@ class Window
 public:
 	void create(const std::string& title)
 	{
+		handle = ec_make_window(msgproc, title);
+	}
 
+	// 이번 단계의 핵심
+	// => 아래 멤버 함수가 static 일수 밖에 없는 이유를 알아야 합니다.
+	static int msgproc(int hwnd, int msg, int a, int b)
+	{
+		switch (msg)
+		{
+		case WM_LBUTTONDOWN: std::cout << "LBUTTON" << hwnd << std::endl; break;
+		case WM_KEYDOWN:     std::cout << "KEYDOWN" << hwnd << std::endl; break;
+		}
+		return 0;
 	}
 };
 
