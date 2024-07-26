8ccf071fefa406b9c65596521720023466b9fc24
ecourse <smkang@codenuri.co.kr>
2024-07-26 10:15:42 +0900

    Add files via upload

--- DAY5/3_GUI6.cpp
+++ DAY5/3_GUI6.cpp
@@ -2,24 +2,37 @@
 #include "cppmaster.h"
 #include <map>
 
+// 클래스 템플릿 만들때
+// => 멤버 함수 안에서 템플릿 인자"T" 를 사용하지 않은 멤버가 있다면
+// => 템플릿이 아닌 기반 클래스를 만들어서 제공한다
 
-class Window
+class WindowBase
 {
+protected:
 	int handle;
+	inline static map<int, WindowBase*> this_map;
+public:
+	void on_lbutton_down() {}
+	void on_key_down() {}
 
-	inline static map<int, Window*> this_map;	
+	// 그외 다양한 멤버 함수들
+	void move() {}
+	void hide() {}
+};
 
+template<typename T>
+class Window : public WindowBase
+{
 public:
 	void create(const std::string& title)
 	{
 		handle = ec_make_window(msgproc, title);
-
 		this_map[handle] = this;
 	}
 
 	static int msgproc(int hwnd, int msg, int a, int b)
 	{
-		Window* self = this_map[hwnd];
+		T* self = static_cast<T*>(this_map[hwnd]);
 
 		switch (msg)
 		{
@@ -38,7 +51,7 @@ public:
 	void on_key_down() {}
 };
 
-class MainWindow : public Window
+class MainWindow : public Window<MainWindow>
 {
 public:
 	void on_lbutton_down() 
